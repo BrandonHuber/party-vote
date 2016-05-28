eb0877e7e9757b87c2b7c4bc0931b8d7f3ceaa3c
CraigRo <rcraig1@harding.edu>
2016-05-28 16:04:48 -0500

    Initial check-in for PartyVote app

---
+++ PartyVote/PartyVote/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace PartyVote
+{
+    [Activity(Label = "Party Vote", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button castVoteButton = FindViewById<Button>(Resource.Id.CastVoteButton);
+            RadioGroup Candidates = FindViewById<RadioGroup>(Resource.Id.radioGroup1);
+
+            castVoteButton.Click += (object sender, EventArgs e) =>
+            {
+                // Cast Vote from Radio Button selection
+                int SelectedCandidate = Candidates.CheckedRadioButtonId;
+                string candidateName = FindViewById<RadioButton>(SelectedCandidate).Text;
+
+                var resultsDialog = new AlertDialog.Builder(this);
+                resultsDialog.SetMessage("Winner: " + candidateName);
+
+                resultsDialog.Show();
+            };
+        }
+    }
+}
+
