206863dc8eb63b23450e3cd278a41244370e923a
CraigRo <rcraig1@harding.edu>
2016-12-05 08:42:46 -0600

    Add placeholder image for main screen.

--- PartyVote/PartyVote/Resources/Resource.Designer.cs
+++ PartyVote/PartyVote/Resources/Resource.Designer.cs
@@ -76,29 +76,32 @@ namespace PartyVote
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060007
-			public const int AddCandidateButton = 2131099655;
+			// aapt resource value: 0x7f060008
+			public const int AddCandidateButton = 2131099656;
+			
+			// aapt resource value: 0x7f06000a
+			public const int BeginElectionButton = 2131099658;
 			
 			// aapt resource value: 0x7f060009
-			public const int BeginElectionButton = 2131099657;
+			public const int CandidateListView = 2131099657;
 			
-			// aapt resource value: 0x7f060008
-			public const int CandidateListView = 2131099656;
-			
-			// aapt resource value: 0x7f060006
-			public const int CandidateTextBox = 2131099654;
+			// aapt resource value: 0x7f060007
+			public const int CandidateTextBox = 2131099655;
 			
 			// aapt resource value: 0x7f060001
 			public const int ElectionTitleTextBox = 2131099649;
 			
-			// aapt resource value: 0x7f060005
-			public const int NewElectionButton = 2131099653;
+			// aapt resource value: 0x7f060006
+			public const int NewElectionButton = 2131099654;
 			
 			// aapt resource value: 0x7f060004
 			public const int PrepareBallotButton = 2131099652;
 			
 			// aapt resource value: 0x7f060003
 			public const int VotingMethodSpinner = 2131099651;
+			
+			// aapt resource value: 0x7f060005
+			public const int imageView1 = 2131099653;
 			
 			// aapt resource value: 0x7f060000
 			public const int textView1 = 2131099648;
